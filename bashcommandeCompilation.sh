c5b5c233c3be535f31c660663a06761d1c810f22
Willian <willian.vieira@usherbrooke.ca>
2024-06-07 13:24:31 -0400

    🔧update compiler script

--- bashcommandeCompilation.sh
+++ bashcommandeCompilation.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
+
+# build intro
+pandoc introduction.md -o introduction.tex \
+    --quiet \
+    --template=templateThesis.tex \
+    --filter pandoc-xnos \
+    --lua-filter=short-captions.lua \
+    --number-sections \
+    --natbib
+
+# build discussion
+pandoc discussion.md -o discussion.tex \
+    --quiet \
+    --template=templateThesis.tex \
+    --filter pandoc-xnos \
+    --lua-filter=short-captions.lua \
+    --number-sections \
+    --natbib
+
+# clean temp files
 rm *aux; rm *blg; rm *.bbl; rm *.lof; rm *lot; rm *.out; rm *.toc
+rm */*aux; rm */*blg; rm */*.bbl; rm */*.lof; rm */*lot; rm */*.out; rm */*.toc
+
+# first render for chapter references
 echo '\includeonly{}' > TheseRef.tex
 cat These.tex >> TheseRef.tex
 pdflatex TheseRef.tex
 bibtex TheseRef
+
+# second render for sections
 cat These.tex > TheseRef.tex
 pdflatex TheseRef.tex
+bibtex introduction
 bibtex chapter1/manuscript_thesis
 bibtex chapter2/manuscript_thesis
 bibtex chapter3/manuscript_thesis
-bibtex annexe1
-bibtex annexe2 # mettre autant d'annexe que nécessaire
+bibtex chapter1/suppInfo_thesis
+bibtex chapter2/suppInfo_thesis
+bibtex chapter3/suppInfo_thesis
+bibtex discussion
 pdflatex TheseRef.tex
 pdflatex TheseRef.tex
 pdflatex TheseRef.tex
-pdflatex TheseRef.tex
+
+# clean temp files
+rm *aux; rm *blg; rm *.bbl; rm *.lof; rm *lot; rm *.out; rm *.toc
+rm */*aux; rm */*blg; rm */*.bbl; rm */*.lof; rm */*lot; rm */*.out; rm */*.toc
