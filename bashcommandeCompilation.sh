32907b49b88ddfa1a323a1acee9f74f35f9390d2
Willian <willian.vieira@usherbrooke.ca>
2024-06-07 16:03:57 -0400

    change order of bib compiler

--- bashcommandeCompilation.sh
+++ bashcommandeCompilation.sh
@@ -20,11 +20,13 @@ pandoc discussion.md -o discussion.tex \
 
 # clean temp files
 rm *aux; rm *blg; rm *.bbl; rm *.lof; rm *lot; rm *.out; rm *.toc
-rm */*aux; rm */*blg; rm */*.bbl; rm */*.lof; rm */*lot; rm */*.out; rm */*.toc
+rm */*aux; rm */*blg; rm */*.bbl
 
 # first render for chapter references
 echo '\includeonly{}' > TheseRef.tex
 cat These.tex >> TheseRef.tex
+pdflatex TheseRef.tex
+bibtex TheseRef
 pdflatex TheseRef.tex
 bibtex TheseRef
 
@@ -35,14 +37,13 @@ bibtex introduction
 bibtex chapter1/manuscript_thesis
 bibtex chapter2/manuscript_thesis
 bibtex chapter3/manuscript_thesis
+bibtex discussion
 bibtex chapter1/suppInfo_thesis
 bibtex chapter2/suppInfo_thesis
 bibtex chapter3/suppInfo_thesis
-bibtex discussion
 pdflatex TheseRef.tex
 pdflatex TheseRef.tex
 pdflatex TheseRef.tex
 
 # clean temp files
-rm *aux; rm *blg; rm *.bbl; rm *.lof; rm *lot; rm *.out; rm *.toc
-rm */*aux; rm */*blg; rm */*.bbl; rm */*.lof; rm */*lot; rm */*.out; rm */*.toc
+rm *aux; rm *blg; rm *.bbl; rm *.lof; rm *.log; rm *lot; rm *.out; rm *.toc; rm TheseRef.tex; rm TheseRef.log
